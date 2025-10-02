775882f3a49d338f989ab920f18ed32d80c52921
Артем Кунин ТКИ-141 <artem.kunin67@gmail.com>
2025-10-02 16:57:19 +0300

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <float.h>
 /*
 *@brief Рассчитывает значение функции по заданным условиям
 *@param x входное значение аргумента
 *@return возвращает рассчитанное значение выбранной функции
 */
-double func(const double x);
+double Func(const double x);
+/*
+*@brief Считывает значение, введенное с клавиатуру с проверкой ввода
+*@return считанное значение
+*/
+double Value();
 /*
 *@brief Точка входа в программу
 *@param x выбранный пользователем значение аргумента
@@ -13,55 +20,48 @@ double func(const double x);
 */
 int main()
 {
-    printf("Программа для решения функции y\n");
+    printf("Программа для решения функции y:\n");
     printf("y = a * x^(2*ln(x)), при 1 <= x <= 2\n");
     printf("y = 1, при x < 1\n");
     printf("y = e^(a*x) * cos(x), при x > 2\n");
     printf("Константа a = 2.0\n\n");
-    double x;
     printf("Введите значение:");
-    if (scanf("%lf",&x) != 1)
-    {
-        printf("Введено неверное значение!\n");
-        return 0;
-    }
-    if (x >= 1 && x <= 2)
-    {
-        printf("Использована формула: y = a * x^(2*ln(x))\n");
-        printf("Итоговое значение:\n");
-        printf("x = %.2f\n",x);
-        printf("y = %lf\n",func(x));
-    
-    }
-    else if (x > 2)
-    {
-        printf("Использована формула: y = e^(a*x) * cos(x)\n");
-        printf("Итоговое значение:\n");
-        printf("x = %.2f\n",x);
-        printf("y = %lf\n",func(x));
-    }
-    else
-    {
-        printf("Использована формула: y = 1\n");
-        printf("Итоговое значение:\n");
-        printf("x = %.2f\n",x);
-        printf("y = %lf\n",func(x));
-    }
+    double x = Value();
+    double result = Func(x);
+    printf("Результат: y(%.2f) = %.2f\n", x, result);
     return 0;
+    
 }
-double func(const double x)
+
+double Func(const double x)
 {
-    const double a = 2;
-    if (x < 1)
+    const double a = 2.0;
+    
+    if (x < 1.0 - DBL_EPSILON)
     {
+        
         return 1.0;
     }
-    if (x >= 1 && x <= 2)
+    else if (x >= 1.0 - DBL_EPSILON && x <= 2.0 + DBL_EPSILON)
     {
-        return a * pow(x,2*log(x));
+        
+        return a * pow(x, 2.0 * log(x));
     }
-    else
+    else 
     {
-        return exp(a*x) * cos(x);
+        
+        return exp(a * x) * cos(x);
     }
+}
+
+double Value()
+{
+    double value = 0;
+    if (!scanf("%lf",&value))
+    {
+        printf("Ошибка! Введено неверное значение!\n");
+
+        abort();
+    }
+    return value;
 }
